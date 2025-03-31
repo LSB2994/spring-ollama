6401c18d9635458bc75848b2cd484735e2e8e987
LSB7 <lengsambthsb2004@gamil.com>
2025-03-31 14:27:48 +0700

    modify controller

--- src/main/java/com/example/spring_ollama/Open.java
+++ src/main/java/com/example/spring_ollama/Open.java
@@ -1,5 +1,7 @@
 package com.example.spring_ollama;
 
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
